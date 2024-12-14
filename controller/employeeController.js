c01b9aefd7f57a48bd0a1e305306ff17ab21d344
Joshua Reyestan <jreyesta@stu.southtexascollege.edu>
2024-12-14 14:53:10 -0600

    Update is working!

--- controller/employeeController.js
+++ controller/employeeController.js
@@ -25,8 +25,24 @@ export const fetch = async(req, res) => {
         if(employees.length === 0) {
             return res.status(404).json({message: "Employees not found!"})
         }
-        
+
         res.status(200).json(employees)
+    } catch (error) {
+        res.status(500).json({error: "Internal Server Error"})
+    }
+}
+
+export const update = async(req, res) => {
+    try {
+        const id = req.params.id
+        const employeeExist = await Employee.findOne({_id:id})
+    
+        if(!employeeExist) {
+            return res.status(404).json({message: "Employee not found"})
+        }
+
+        const updateEmployee = await Employee.findByIdAndUpdate(id, req.body, {new: true}) 
+        res.status(201).json(updateEmployee)
     } catch (error) {
         res.status(500).json({error: "Internal Server Error"})
     }
